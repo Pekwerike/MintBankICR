fe27d85fd118eaa2b8a3b07b35977347bc77d458
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-09 11:48:12 +0100

    Added runtime requests for MintBankICR to use the device camera

--- app/src/main/java/com/pekwerike/mintbankicr/MainActivity.kt
+++ app/src/main/java/com/pekwerike/mintbankicr/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.pekwerike.mintbankicr
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +10,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.pekwerike.mintbankicr.ui.theme.MintBankICRTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +20,44 @@ class MainActivity : ComponentActivity() {
             MintBankICRTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    // confirm if permission to use the camera has been granted by the user
+    private fun isCameraPermissionGranted() = ActivityCompat
+            .checkSelfPermission(this, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MintBankICRTheme {
-        Greeting("Android")
+    //request for camera permission
+    private fun requestCameraPermission(){
+         if(isCameraPermissionGranted()){
+             // start camera preview
+         }else {
+             ActivityCompat.requestPermissions(this, arrayOf(CAMERA_PERMISSION),
+             CAMERA_PERMISSION_REQUEST_CODE)
+         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == CAMERA_PERMISSION_REQUEST_CODE){
+            if(isCameraPermissionGranted()){
+                // start camera preview
+            }else {
+                // TODO manage the scenario when a user denies the permission parmenently
+                requestCameraPermission()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        const val CAMERA_PERMISSION = Manifest.permission.CAMERA
     }
 }
