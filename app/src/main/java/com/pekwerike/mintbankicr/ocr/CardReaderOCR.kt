4f3459da0dc0103d45ac3d127dc5dea882424fb0
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-09 13:44:12 +0100

    Building extractCreditCardNumber function in CardReaderOCR

--- app/src/main/java/com/pekwerike/mintbankicr/ocr/CardReaderOCR.kt
+++ app/src/main/java/com/pekwerike/mintbankicr/ocr/CardReaderOCR.kt
@@ -5,12 +5,19 @@ import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import java.nio.ByteBuffer
 
 class CardReaderOCR(private val activityContext: Context) : ImageAnalysis.Analyzer{
     override fun analyze(image: ImageProxy) {
         // get the byteBuffer of the image and parse it to ML Kit
         val imageByteBuffer = image.planes[0].buffer
+        image.imageInfo.rotationDegrees
         image.close()
+    }
+
+    private fun extractCreditCardNumber(imageByteBuffer : ByteBuffer, imageRotationDegrees : Int){
+        InputImage.fromByteBuffer(imageByteBuffer, 480, 360, imageRotationDegrees)
     }
 
 }
